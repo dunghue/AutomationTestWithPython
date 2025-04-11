c9facc52dced9f10ff256b16991516c9990bce2f
dunghue <vobuidunghue@gmail.com>
2025-04-11 21:44:39 +0700

    Lesson 2.1-2.5

---
+++ hello_world.py
@@ -0,0 +1,22 @@
+print("Hello World");
+fruit = ["apple", "orange", "banana", "mango", "strawbery", "peaches"];
+x = "This apple is tasty";
+print(fruit[2:4]);
+print(x[2:8]);
+
+score = {
+    "John": 95,
+    "Rita": 90,
+    "Sam": 85,  
+}
+print("John's score is", score["John"]);
+
+car = ["toyota", "ford", "tesla", "truck", "mercedes"];
+for x in car:
+    if x == "tesla":
+        print("I wish to own tesla one day");
+    elif x == "truck":
+        print("But trucks are the best for work");
+        
+    else:
+        print("I like", x, "too");
